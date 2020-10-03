0a6b15cec9a40140a64d3535fcdd0222a5efe858
Pedro González <pgonzalez@post.com>
2020-10-03 03:48:15 -0500

    Agregar retos de funciones

---
+++ 07_Funciones/retos.js
@@ -0,0 +1,13 @@
+console.log('Hola desde retos!');
+
+//RETO 1
+//Crear con funciones de tipo flecha o arrow functions () => {}
+//Una función que reste dos números
+//Una función que multiplique dos números
+
+
+//RETO 2
+//Una función que le pregunte al usuario dos valores y que regrese:
+//El área de un cuadrado
+//El área de un rectángulo
+//El área de un triángulo
