08b13ba6dcf007d958e71a41aa7973d95302f4c5
Pedro González <pgonzalez@post.com>
2020-10-08 21:19:19 -0500

    Agregar reto pizza

---
+++ 10_DOM_JavaScript/RetoPizza/app.js
@@ -0,0 +1,15 @@
+console.log('Hola desde Reto Pizza! 🍕');
+
+//-------------------> Challenge <------------------
+// * Usar Bootstrap *
+// Preguntar Nombre
+// Preguntar Dirección
+// Preguntar Teléfono
+// Preguntar si quieren pizza con piña o sin
+// respuestas: sí o no
+// -> mostrar las respuestas en una card de Bootstrap después de presionar un botón
+
+// PASOS SUGERIDOS
+// Paso 1: Declarar los elementos (inputs y respuestas) que vamos a usar del html (getElementById('id') es una forma)
+// Paso 2: Declarar la función que generará la card con las entradas del usuario
+// Paso 3: Agregar el event listener del botón para que ejecuté la función que genere la card
